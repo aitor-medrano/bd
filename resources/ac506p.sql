f30257f9d0421a8159ce265bdc63ee52324b6b4f
Aitor Medrano <aitormedrano@gmail.com>
2024-10-08 10:35:27 +0200

    Deployed 641336a with MkDocs version: 1.6.1

---
+++ resources/ac506p.sql
@@ -0,0 +1,24 @@
+-- Creación de la tabla DEPARTAMENTO en PostgreSQL
+CREATE TABLE DEPARTAMENTO (
+    codD SERIAL, -- autoincrementable
+    nombre VARCHAR(50) NOT NULL,
+    direcc VARCHAR(100),
+    
+    -- Restricciones
+    CONSTRAINT PK_DEPARTAMENTO PRIMARY KEY (codD)
+);
+
+-- Creación de la tabla EMPLEADO en PostgreSQL
+CREATE TABLE EMPLEADO (
+    dni VARCHAR(9),
+    nombrec VARCHAR(100) NOT NULL,
+    salario DECIMAL(10, 2) DEFAULT 1000,
+    direcc VARCHAR(100),
+    departamento INT NOT NULL,
+    
+    -- Restricciones
+    CONSTRAINT PK_EMPLEADO PRIMARY KEY (dni),
+    CONSTRAINT UK_NOMBREC UNIQUE (nombrec),
+    CONSTRAINT CK_SALARIO CHECK (salario >= 900),
+    CONSTRAINT FK_DEPARTAMENTO FOREIGN KEY (departamento) REFERENCES DEPARTAMENTO(codD)
+);
