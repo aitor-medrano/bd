432258276c54ee622422bfa9e9f35e96fc5597bb
Aitor Medrano <a.medrano@edu.gva.es>
2025-03-03 16:58:59 +0100

    Deployed 641336a with MkDocs version: 1.6.1

--- soluciones/procedimientos.sql
+++ soluciones/procedimientos.sql
@@ -12,12 +12,15 @@ cantidadDepartamentos
 
 
 
-DELIMITER //
-CREATE OR REPLACE PROCEDURE generaCodigo(parteUno VARCHAR(32), parteDos VARCHAR(32))
-    COMMENT "Genera un código con los tres primero de parteUno y los tres ultimos de parte dos"
-BEGIN
-    -- Cuenta los departamentos
-    SELECT concat( parteUno, parteDos );
-END
-//
-DELIMITER ;
+    DELIMITER //
+    CREATE OR REPLACE PROCEDURE generaCodigo(parteUno VARCHAR(32), parteDos VARCHAR(32), OUT resultado VARCHAR(6))
+        COMMENT "Genera un código con los tres primero de parteUno y los tres ultimos de parte dos"
+    BEGIN
+        -- Cuenta los departamentos
+        SET resultado = concat( substring(parteUno,1,3) , substring(parteDos,-3) );
+    END
+    //
+    DELIMITER ;
+
+    call generaCodigo("Diego","Jonathan", @tarde);
+    select @tarde;
